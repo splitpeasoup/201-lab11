f046e49320bf74594fe18aa4f6b2f7aec7228f40
splitpeasoup <caitlinsweeneylmt@gmail.com>
2017-11-17 21:51:36 -0800

    Add files via upload
    still trying to have chart work

--- busmall-start/busmall.js
+++ busmall-start/busmall.js
@@ -1,16 +1,13 @@
 'use strict';
-
-
 Product.names = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can',
 'wine-glass'];
-
 Product.all = [];
 Product.container = document.getElementById('image_container');
 Product.justViewed = [];
 Product.pics = [document.getElementById('left'), document.getElementById('center'), document.getElementById('right')];
 Product.tally = document.getElementById('tally');
 Product.totalClicks = 0;
-
+var votes = [];
 function Product(name) {
   this.name = name;
   this.path = 'img/' + name + '.jpg';
@@ -21,11 +18,9 @@ function Product(name) {
 for(var i = 0; i < Product.names.length; i++) {
   new Product(Product.names[i]);
 }
-
 function makeRandom() {
   return Math.floor(Math.random() * Product.names.length);
 }
-
 function displayPics() {
   var currentlyShowing = [];
   currentlyShowing[0] = makeRandom();
@@ -54,7 +49,6 @@ function displayPics() {
   }
 }
 //handle click events
-
 function handleClick(event) {
   console.log(Product.totalClicks, 'total clicks');
   if(Product.totalClicks >= 24) {
@@ -80,130 +74,61 @@ function showTally() {
     liEl.textContent = Product.all[i].name + ' has ' + Product.all[i].votes + ' votes in ' + Product.all[i].views + ' views';
     Product.tally.appendChild(liEl);
   }
+  createChart();
 }
 //event listener
 Product.container.addEventListener('click', handleClick);
 displayPics();
-
-
-var data = Product.all;
-var color = Chart.helpers.color;
-var horizontalBarChartData = {
-    labels: Product.names,
-    datasets: [{
-        label: 'Dataset 1',
-        backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
-        borderColor: window.chartColors.red,
-        borderWidth: 1,
-        data: [
-            randomScalingFactor(),
-            randomScalingFactor(),
-            randomScalingFactor(),
-            randomScalingFactor(),
-            randomScalingFactor(),
-            randomScalingFactor(),
-            randomScalingFactor()
-        ]
-    }, {
-        label: 'Dataset 2',
-        backgroundColor: color(window.chartColors.blue).alpha(0.5).rgbString(),
-        borderColor: window.chartColors.blue,
-        data: [
-            randomScalingFactor(),
-            randomScalingFactor(),
-            randomScalingFactor(),
-            randomScalingFactor(),
-            randomScalingFactor(),
-            randomScalingFactor(),
-            randomScalingFactor()
-        ]
-    }]
-
-};
-
-window.onload = function() {
-    var ctx = document.getElementById("canvas").getContext("2d");
-    window.myHorizontalBar = new Chart(ctx, {
-        type: 'horizontalBar',
-        data: horizontalBarChartData,
-        options: {
-            // Elements options apply to all of the options unless overridden in a dataset
-            // In this case, we are setting the border of each horizontal bar to be 2px wide
-            elements: {
-                rectangle: {
-                    borderWidth: 2,
-                }
-            },
-            responsive: true,
-            legend: {
-                position: 'right',
-            },
-            title: {
-                display: true,
-                text: 'Chart.js Horizontal Bar Chart'
-            }
-        }
-    });
-
-};
-
-// document.getElementById('randomizeData').addEventListener('click', function() {
-//     var zero = Math.random() < 0.2 ? true : false;
-//     horizontalBarChartData.datasets.forEach(function(dataset) {
-//         dataset.data = dataset.data.map(function() {
-//             return zero ? 0.0 : randomScalingFactor();
-//         });
-
-//     });
-//     window.myHorizontalBar.update();
-// });
-
-var colorNames = Object.keys(window.chartColors);
-
-// document.getElementById('addDataset').addEventListener('click', function() {
-//     var colorName = colorNames[horizontalBarChartData.datasets.length % colorNames.length];;
-//     var dsColor = window.chartColors[colorName];
-//     var newDataset = {
-//         label: 'Dataset ' + horizontalBarChartData.datasets.length,
-//         backgroundColor: color(dsColor).alpha(0.5).rgbString(),
-//         borderColor: dsColor,
-//         data: []
-//     };
-
-//     for (var index = 0; index < horizontalBarChartData.labels.length; ++index) {
-//         newDataset.data.push(randomScalingFactor());
-//     }
-
-//     horizontalBarChartData.datasets.push(newDataset);
-//     window.myHorizontalBar.update();
-// });
-
-// document.getElementById('addData').addEventListener('click', function() {
-//     if (horizontalBarChartData.datasets.length > 0) {
-//         var month = MONTHS[horizontalBarChartData.labels.length % MONTHS.length];
-//         horizontalBarChartData.labels.push(month);
-
-//         for (var index = 0; index < horizontalBarChartData.datasets.length; ++index) {
-//             horizontalBarChartData.datasets[index].data.push(randomScalingFactor());
-//         }
-
-//         window.myHorizontalBar.update();
-//     }
-// });
-
-// document.getElementById('removeDataset').addEventListener('click', function() {
-//     horizontalBarChartData.datasets.splice(0, 1);
-//     window.myHorizontalBar.update();
-// });
-
-// document.getElementById('removeData').addEventListener('click', function() {
-//     horizontalBarChartData.labels.splice(-1, 1); // remove the label first
-
-//     horizontalBarChartData.datasets.forEach(function (dataset, datasetIndex) {
-//         dataset.data.pop();
-//     });
-
-//     window.myHorizontalBar.update();
-// });
-
-
+function createChart() {
+  for(var i = 0; i < Product.names.length; i++) {
+    votes.push(Product.all[i].votes);
+  }
+  //this holds the value for the votes of each product image
+  var labelColors = [
+    'red',
+    'blue',
+    'yellow',
+    'green',
+    'purple',
+    'orange',
+    'red',
+    'blue',
+    'yellow',
+    'green',
+    'purple',
+    'orange',
+    'red',
+    'blue',
+    'yellow',
+    'green',
+    'purple',
+    'orange',
+    'red',
+    'blue',
+    'yellow',
+    'green',
+    'purple',
+    'orange'
+  ];
+  var ctx = document.getElementById('chart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: Product.names,
+      datasets: [{
+        label: '# of Votes',
+        data: votes,
+        backgroundColor: labelColors
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+}
